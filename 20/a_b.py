ffed82865ecbcbadff03f95d2ef39972dd15974b
partyfowl <14924988+partyfowl@users.noreply.github.com>
2024-12-20 21:00:25 +0000

    more efficiency

--- 20/a_b.py
+++ 20/a_b.py
@@ -12,8 +12,11 @@ def run_race(
     grid: list[str],
     start: complex,
     end: complex,
-):
-    path_taken: list[complex] = [start]
+) -> dict[complex, int]:
+    index = 0
+
+    # As of Python 3.7, regular dicts are guaranteed to be ordered.
+    path_taken: dict[complex, int] = {start: index}
     position = start
 
     while position != end:
@@ -25,63 +28,71 @@ def run_race(
                 break
 
         while grid[int(next_position.real)][int(next_position.imag)] != "#":
+            index += 1
             position = next_position
-            path_taken.append(position)
+            path_taken[position] = index
             next_position += direction
 
     return path_taken
 
 
-def main(part: int):
-    with open("input.txt") as f:
-        grid = [_.strip() for _ in f.readlines()]
-
-    end = next(
-        complex(y, line.index("E")) for y, line in enumerate(grid) if "E" in line
-    )
-    start = next(
-        complex(y, line.index("S")) for y, line in enumerate(grid) if "S" in line
-    )
-
-    vanilla_path = run_race(grid, start, end)  # 85 - off by one but this is fine
-
-    if part == 1:
-        cheat_duration = 2
-    elif part == 2:
-        cheat_duration = 20
-
+def solve(max_cheat_duration: int, path: dict[complex, int]):
     possible_cheats = {
         sum(_)
-        for _ in combinations_with_replacement((1, -1, 0, 1j, -1j), r=cheat_duration)
+        for _ in combinations_with_replacement(
+            (1, -1, 0, 1j, -1j), r=max_cheat_duration
+        )
     }
 
     total = 0
 
     # Sets are faster than lists for lookups
-    path_forwards = set(vanilla_path)
+    path_forwards = set(path)
 
-    # Create this dict once to avoid calling vanilla_path.index many times
-    location_index = {k: v for v, k in enumerate(vanilla_path)}
-
-    for i, location in enumerate(vanilla_path):
+    for i, location in enumerate(path):
         path_forwards.remove(location)
 
         for cheat in possible_cheats:
             cheat_location = location + cheat
             if cheat_location in path_forwards:
 
-                time_saved = (
-                    location_index[cheat_location] - i
-                ) - get_cheat_distance_travelled(cheat)
+                time_saved = (path[cheat_location] - i) - get_cheat_distance_travelled(
+                    cheat
+                )
 
                 if time_saved >= 100:
                     total += 1
 
-    print(f"Part {part}: {total}")
+    return total
+
+
+def main():
+    with open("input.txt") as f:
+        grid = []
+        line = ""
+        y = 0
+        x = 0
+        while char := f.read(1):
+            if char == "\n":
+                grid.append(line)
+                line = ""
+                y += 1
+                x = 0
+            else:
+                line += char
+                if char == "E":
+                    end = complex(y, x)
+                elif char == "S":
+                    start = complex(y, x)
+                x += 1
+        grid.append(line)
+
+    path = run_race(grid, start, end)  # 85 - off by one but this is fine
+
+    print("Part 1:", solve(2, path))
+    print("Part 2:", solve(20, path))
 
 
 if __name__ == "__main__":
-    print(timeit(lambda: main(1), number=1))
-    # 0.5023840590001782 seconds
-    print(timeit(lambda: main(2), number=1))
-    # 1.6199745970006916 seconds
+    print(timeit(main, number=1))
+    # 1.1562933510067523 seconds
