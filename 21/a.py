021edcd23fc044c49cd41265a180f035a68e50b8
partyfowl <14924988+partyfowl@users.noreply.github.com>
2024-12-21 15:43:51 +0000

    part 1 solution

---
+++ 21/a.py
@@ -0,0 +1,100 @@
+from functools import cache
+from itertools import permutations, product
+from timeit import timeit
+
+DOOR_KEYPAD = {
+    "7": 0 + 0j,
+    "8": 0 + 1j,
+    "9": 0 + 2j,
+    "4": 1 + 0j,
+    "5": 1 + 1j,
+    "6": 1 + 2j,
+    "1": 2 + 0j,
+    "2": 2 + 1j,
+    "3": 2 + 2j,
+    # nothing at 3 + 0 j
+    None: 3 + 0j,
+    "0": 3 + 1j,
+    "A": 3 + 2j,
+}
+
+ROBOT_KEYPAD = {
+    # nothing at 0 + 0 j
+    None: 0 + 0j,
+    "^": 0 + 1j,  # ^
+    "A": 0 + 2j,
+    "<": 1 + 0j,  # <
+    "v": 1 + 1j,  # v
+    ">": 1 + 2j,  # >
+}
+
+DIRECTIONS = {"^": -1, ">": 1j, "v": 1, "<": -1j}
+
+
+@cache
+def find_paths(position: complex, destination: complex, avoid: complex):
+    movement = destination - position
+
+    movements = []
+
+    for _ in range(abs(int(movement.real))):
+        if movement.real > 0:
+            movements.append("v")
+        else:
+            movements.append("^")
+
+    for _ in range(abs(int(movement.imag))):
+        if movement.imag > 0:
+            movements.append(">")
+        else:
+            movements.append("<")
+
+    paths = set("".join(_) for _ in permutations(movements, len(movements)))
+
+    start_position = position
+    viable_paths = set()
+
+    for path in paths:
+        position = start_position
+        for direction in path:
+            position += DIRECTIONS[direction]
+            if position == avoid:
+                break
+        else:  # nobreak
+            viable_paths.add(path + "A")
+
+    return viable_paths
+
+
+def robot_recurse(code: str, keypad: dict[str, complex], remaining_robots: int) -> int:
+    if remaining_robots == 0:
+        return len(code)
+
+    position = keypad["A"]
+
+    paths = []
+
+    for digit in code:
+        paths.append(find_paths(position, keypad[digit], keypad[None]))
+        position = keypad[digit]
+
+    return min(
+        robot_recurse("".join(options), ROBOT_KEYPAD, remaining_robots - 1)
+        for options in (tuple(product(*paths)))
+    )
+
+
+def main():
+    with open("input.txt") as f:
+        codes = f.read().splitlines()
+
+    total = 0
+    for code in codes:
+        total += robot_recurse(code, DOOR_KEYPAD, 3) * int(code.strip("A"))
+
+    print(total)
+
+
+if __name__ == "__main__":
+    print(timeit(main, number=1))
+    # 6.961681760996726
